79699b950c0ca6c7613046b1eb8f3242dba2df63
Shikha Gupta <Shikha@apples-MacBook-Pro-4.local>
2016-11-10 16:56:47 +0530

    Initial commit

---
+++ Where am I/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Where am I
+//
+//  Created by Shikha Gupta on 10/11/16.
+//  Copyright © 2016 shikha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
